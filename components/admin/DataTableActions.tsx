3e73fdffac43cde080c18591864a0d5f08a3ef30
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2024-11-30 19:12:25 +0100

    refactor: reusable data table actions

---
+++ components/admin/DataTableActions.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState, useTransition } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+
+import BillboardAEDialogContent from "@/components/admin/billboards/BillboardAEDialogContent";
+
+import { PencilLine, Trash2 } from "lucide-react";
+
+interface DeleteProps {
+  deleteTitle: string;
+  deleteDescription: string;
+  onDelete: () => void;
+}
+
+type DialogContentProps = {
+  item: any;
+  closeDialog: () => void;
+};
+
+interface DatatableActionsProps {
+  item: any;
+  deleteProps: DeleteProps;
+  DialogContent: React.ComponentType<DialogContentProps>;
+}
+
+export default function DataTableActions({
+  item,
+  deleteProps,
+  DialogContent,
+}: DatatableActionsProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isProcessing, startProcessing] = useTransition();
+
+  const handleDelete = () => {
+    startProcessing(async () => {
+      deleteProps.onDelete();
+    });
+  };
+  return (
+    <div className="flex justify-end gap-2">
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Button variant={"secondary"} className="size-10 hover:bg-primary/30">
+            <PencilLine />
+          </Button>
+        </DialogTrigger>
+        <DialogContent item={item} closeDialog={() => setIsOpen(false)} />
+      </Dialog>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button
+            variant={"secondary"}
+            className="group size-10 hover:bg-destructive/30"
+          >
+            <Trash2 className="text-destructive" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{deleteProps.deleteTitle}</AlertDialogTitle>
+            <AlertDialogDescription>
+              {deleteProps.deleteDescription}
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={isProcessing}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-destructive hover:bg-destructive/80"
+              onClick={handleDelete}
+              disabled={isProcessing}
+            >
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+}
--- components/admin/categories/table/CategoriesTableActions.tsx
+++ components/admin/categories/table/CategoriesTableActions.tsx
@@ -3,30 +3,14 @@
 import { deleteCategory } from "@/actions/category";
 
 import { useRouter } from "next/navigation";
-import { useState, useTransition } from "react";
 
 import { useStore } from "@/hooks/use-store";
 import { toast } from "@/hooks/use-toast";
 
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-
 import CategoryAEDialogContent from "@/components/admin/categories/CategoryAEDialogContent";
+import DataTableActions from "@/components/admin/DataTableActions";
 
 import { Category } from "@prisma/client";
-
-import { PencilLine, Trash2 } from "lucide-react";
 
 interface CategoryTableActionsProps {
   category: Category;
@@ -35,77 +19,34 @@ interface CategoryTableActionsProps {
 export default function CategoryTableActions({
   category,
 }: CategoryTableActionsProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isProcessing, startProcessing] = useTransition();
-
   const store = useStore();
   const router = useRouter();
 
-  const onDelete = () => {
-    startProcessing(async () => {
-      deleteCategory(store.id, category.id).then((res) => {
-        if (res.success) {
-          toast({
-            title: "Category deleted",
-            description: "The category has been successfully deleted.",
-          });
-          router.refresh();
-        } else {
-          toast({
-            title: "An error occurred during category deletion",
-            description: res.error,
-          });
-        }
-      });
-    });
-  };
-
   return (
-    <div className="flex justify-end gap-2">
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogTrigger asChild>
-          <Button variant={"secondary"} className="size-10 hover:bg-primary/30">
-            <PencilLine />
-          </Button>
-        </DialogTrigger>
-        <CategoryAEDialogContent
-          item={category}
-          closeDialog={() => setIsOpen(false)}
-        />
-      </Dialog>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button
-            variant={"secondary"}
-            className="group size-10 hover:bg-destructive/30"
-          >
-            <Trash2 className="text-destructive" />
-          </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>
-              Are you sure you want to delete this category?
-            </AlertDialogTitle>
-            <AlertDialogDescription>
-              The category will be removed and all assossiated items will be
-              unlinked.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel disabled={isProcessing}>
-              Cancel
-            </AlertDialogCancel>
-            <AlertDialogAction
-              className="bg-destructive hover:bg-destructive/80"
-              onClick={onDelete}
-              disabled={isProcessing}
-            >
-              Delete
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
+    <DataTableActions
+      item={category}
+      deleteProps={{
+        deleteTitle: "Are you sure you want to delete this category?",
+        deleteDescription:
+          "The category will be removed and all assossiated items will be unlinked.",
+        onDelete: () => {
+          deleteCategory(store.id, category.id).then((res) => {
+            if (res.success) {
+              toast({
+                title: "Category deleted",
+                description: "The category has been successfully deleted.",
+              });
+              router.refresh();
+            } else {
+              toast({
+                title: "An error occurred during category deletion",
+                description: res.error,
+              });
+            }
+          });
+        },
+      }}
+      DialogContent={CategoryAEDialogContent}
+    />
   );
 }
